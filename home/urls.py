ad566f2ca0ae27f2f192551f10e67089c67cbedf
agarjun007 <agarjun007@gmail.com>
2021-09-21 14:18:36 +0530

    finished

--- home/urls.py
+++ home/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
-from .views import home
+from .views import home,add_cart,cart
 urlpatterns=[
     path('',home,name='home'),
+    path('add_cart<int:id>/',add_cart,name='add_cart'),
+    path('cart/',cart,name='cart')
 ]
--- home/views.py
+++ home/views.py
@@ -1,6 +1,25 @@
 from django.shortcuts import render,redirect
 from adminpanel.models import Products
+import json
 
 def home(request):
     products = Products.objects.all()
     return render(request,'home.html',{'products':products})
+
+cart_list = []
+def add_cart(request,id):
+    response = redirect('/')
+    cart_list.append(id)
+    cart = json.dumps(cart_list)
+    response.set_cookie('product_ids',cart)    
+    return response
+
+def cart(request):
+    product_ids = request.COOKIES.get('product_ids') 
+    print('product idss',product_ids)
+    product_ids=json.loads(product_ids)
+    products=[]
+    for id in product_ids: 
+        product = Products.objects.get(id=id)
+        products.append(product)
+    return render(request,'cart.html',{'products':products})
